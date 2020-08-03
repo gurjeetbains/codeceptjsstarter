9f2f7eee2d614811b59e1c083b18188f740fd0f3
Gurjeet Bains <gurjeetbains45@gmail.com>
2020-08-03 14:06:46 +1200

    first commit

---
+++ multi_codecept.conf.js
@@ -0,0 +1,78 @@
+const {
+  setHeadlessWhen
+} = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './tests/*_test.js',
+  // output: './output',// This is used for webdriver output
+  multiple: {
+    parallel: {
+      // Splits tests into 2 chunks
+      chunks: 2
+    }
+  },
+  helpers: {
+    WebDriver: {
+      url: 'https://myapp.com',
+      browser: 'chrome',
+      host: 'localhost',
+      port: 4444,
+      restart: false,
+      windowSize: '1920x1680',
+      desiredCapabilities: {
+        chromeOptions: {
+          args: [ /*"--headless",*/ "--disable-gpu", "--no-sandbox"]
+        }
+      }
+    },
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {
+    reporterOptions: {
+      "codeceptjs-cli-reporter": {
+        "stdout": "-",
+        "options": {
+          "verbose": false,
+          "steps": true,
+        }
+      },
+      "mochawesome": {
+        "stdout": "./logs/test_runs.log",
+        "options": {
+          "reportDir": "./mochareport",
+          "reportFilename": "report"
+        },
+        "mocha-junit-reporter": {
+          "stdout": "./logs/test_runs.log",
+          "options": {
+            "mochaFile": "./output/result.xml"
+          },
+          "attachments": true //add screenshot for a failed test
+        }
+      }
+    }
+  },
+  name: 'codecepttest',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    },
+    allure: {
+      enabled: true
+    },
+    wdio: {
+      enabled: true,
+      services: ['selenium-standalone']
+    }
+  }
+}
---
+++ tests/demo2_test.js
@@ -0,0 +1,6 @@
+Feature('My second Test');
+
+Scenario('test something new', (I) => {
+  I.amOnPage('https://github.com');
+  I.see('GitHubSSS');
+});
